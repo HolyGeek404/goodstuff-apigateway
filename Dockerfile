7ac971f32c1f767e8c0643d250eba1311a3a0d33
HolyGeek404 <wmatkowski404@proton.me>
2026-02-07 19:23:10 +0100

    Added Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+﻿FROM mcr.microsoft.com/dotnet/sdk:10.0 AS build
+ARG BUILD_CONFIGURATION=Release
+WORKDIR /src
+COPY ["GoodStuff.ApiGateway/GoodStuff.ApiGateway.csproj", "GoodStuff.ApiGateway/"]
+RUN dotnet restore "GoodStuff.ApiGateway/GoodStuff.ApiGateway.csproj"
+COPY . .
+WORKDIR "/src/GoodStuff.ApiGateway"
+RUN dotnet publish "./GoodStuff.ApiGateway.csproj" -c $BUILD_CONFIGURATION -o /app/publish --no-restore /p:UseAppHost=false
+
+FROM mcr.microsoft.com/dotnet/aspnet:10.0 AS final
+USER $APP_UID
+WORKDIR /app
+
+COPY --from=build /app/publish .
+ENTRYPOINT ["dotnet", "GoodStuff.ApiGateway.dll"]
--- GoodStuff.ApiGateway/Program.cs
+++ GoodStuff.ApiGateway/Program.cs
@@ -5,25 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot();
-
 builder.Services.AddOpenApi();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: "FrontendPolicy",
-        policy  =>
-        {
-            policy.WithOrigins("http://localhost:4200") // Angular origin
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-        });
-});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-app.UseCors("FrontendPolicy");
 app.UseHttpsRedirection();
 await app.UseOcelot();
 app.Run();
